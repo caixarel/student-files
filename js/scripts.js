ff7a66ce164cf46b26bd36687e9c01299b6e81f0
caixarel <44868812+caixarel@users.noreply.github.com>
2021-07-11 19:20:41 +0200

    safary bug solved

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ const onlyNumbers=/\D+/g;
 const birthdayRegEx= /^(\d{4})(\d{2})(\d{2})(\d+)/;
 
 function profileText(data){
-    const person= data.results[0];
+    const person= data;
     const profileToAppend =`<div class="card">
     <div class="card-img-container">
         <img class="card-img" src="${person.picture.medium}" alt="profile picture">
@@ -21,15 +21,14 @@ function profileText(data){
     gallery.append(profileToAppend);
 }
 
-for(let i=0;i<12;i++){
-    $.ajax({
-        url: 'https://randomuser.me/api/',
-        dataType: 'json',
-        success: function(data) {
-            profiles.push(data);
-        }
-      });
-}
+$.ajax({
+    url: "https://randomuser.me/api?results=12",
+}).done(function(data) {
+    data.results.forEach(user => {
+        profiles.push(user);
+    });
+});
+
 $( document ).ajaxStop(function() {
     for(let i = 0;i<profiles.length;i++){
         profileText(profiles[i]);
@@ -44,35 +43,13 @@ function showModal(){
         cards[i].addEventListener('click',()=>{
             modalText(i);
             profileIndex = i;
-            //modalButtonsFunctions(i);
         })
     }
-
  }
 
-//  function modalButtonsFunctions(i){
-//     let modalCloseButton = document.getElementById('modal-close-btn');
- 
-//     let bodyDiv =modalCloseButton.parentNode.parentNode.parentNode;
-//    const prevButton = document.getElementById('modal-prev');
-//     const nextButton = document.getElementById('modal-next');
-//     nextButton.addEventListener('click',()=>{
-//         bodyDiv.removeChild(bodyDiv.children[2]);
-//         modalText(i+1);
-//     })
-//     prevButton.addEventListener('click',()=>{
-//         bodyDiv.removeChild(bodyDiv.children[2]);
-//         modalText(i-1);
-//     })
-
-//     modalCloseButton.addEventListener('click',(e)=>{
-//   //  bodyDiv.removeChild(bodyDiv.children[2]);
-//     console.log('botao');
-//     }) 
-//  }
 
  function modalText(i){
-    const result=profiles[i].results[0];
+    const result=profiles[i];
     const number =result.phone.replace(onlyNumbers,"");
     const phonenumber =number.replace(phoneRegEx," ($1) $2-$3");
     const birthdayFormatted= result.dob.date.replace(onlyNumbers,"").replace(birthdayRegEx,"$2/$3/$1");
@@ -81,7 +58,7 @@ function showModal(){
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
                 <img class="modal-img" src="${result.picture.medium}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${result.name.first} ${profiles[i].results[0].name.last}</h3>
+                <h3 id="name" class="modal-name cap">${result.name.first} ${result.name.last}</h3>
                 <p class="modal-text">${result.email}</p>
                 <p class="modal-text cap">${result.location.city}</p>
                 <hr>
@@ -107,13 +84,13 @@ function showModal(){
         bodyDiv.removeChild(bodyDiv.children[2]);
      }
      else if(e.target.id === 'modal-next'){
-        bodyDiv.removeChild(bodyDiv.children[2]);
         profileIndex++;
         if(profileIndex>profiles.length-1){
             profileIndex=0;
         }
+        bodyDiv.removeChild(bodyDiv.children[2]);
         modalText(profileIndex);
-
+        console.log('dadasdasd')
      }
      else if(e.target.id === 'modal-prev'){
         bodyDiv.removeChild(bodyDiv.children[2]);
